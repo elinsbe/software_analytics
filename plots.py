da6d0cd4fdcb5bb12b1a2302e418ed52cb99ce24
Elin Skaanlund Berntsen <elinsberntsen@gmail.com>
2026-03-26 10:36:44 +0100

    add name of repo in plot

--- plots.py
+++ plots.py
@@ -6,7 +6,7 @@ import os
 # files
 
 
-def duplication(df_ai, df_no_ai, path):
+def duplication(df_ai, df_no_ai, path, repo):
     mean_change_ai = df_ai["duplication_percentage"].mean()
     mean_change_no_ai = df_no_ai["duplication_percentage"].mean()
 
@@ -28,13 +28,13 @@ def duplication(df_ai, df_no_ai, path):
 
     plt.xticks([1, 2], ["AI", "Non-AI"])
     plt.ylabel("Duplication percentage")
-    plt.title("Distribution of duplication percentage")
+    plt.title(f"Distribution of duplication percentage in {repo}")
 
     plt.savefig(f"{path}/duplication.png")
     plt.clf()
 
 
-def changes_in_30_days(df_ai, df_no_ai, path):
+def changes_in_30_days(df_ai, df_no_ai, path, repo):
     mean_change_ai = df_ai["changes_in_30_days"].mean()
     mean_change_no_ai = df_no_ai["changes_in_30_days"].mean()
 
@@ -58,13 +58,13 @@ def changes_in_30_days(df_ai, df_no_ai, path):
 
     plt.xticks([1, 2], ["AI", "Non-AI"])
     plt.ylabel("Number of changes in 30 days")
-    plt.title("Distribution of changes in 30 days")
+    plt.title(f"Distribution of changes in 30 days in {repo}")
 
     plt.savefig(f"{path}/changes_in_30_days.png")
     plt.clf()
 
 
-def files(df_ai, df_no_ai, path):
+def files(df_ai, df_no_ai, path, repo):
     mean_value_AI = df_ai["files"].mean()
     mean_value_NO_AI = df_no_ai["lines"].mean()
 
@@ -83,13 +83,13 @@ def files(df_ai, df_no_ai, path):
 
     plt.xticks([1, 2], ["AI", "Non-AI"])
     plt.ylabel("Files changed")
-    plt.title("Distribution of files changed")
+    plt.title(f"Distribution of files changed in {repo}")
 
     plt.savefig(f"{path}/files.png")
     plt.clf()
 
 
-def mean_lines(df_ai, df_no_ai, path):
+def mean_lines(df_ai, df_no_ai, path, repo):
     mean_value_AI = df_ai["lines"].mean()
     mean_value_NO_AI = df_no_ai["lines"].mean()
 
@@ -108,13 +108,13 @@ def mean_lines(df_ai, df_no_ai, path):
 
     plt.xticks([1, 2], ["AI", "Non-AI"])
     plt.ylabel("Lines changed")
-    plt.title("Distribution of Lines changed")
+    plt.title(f"Distribution of Lines changed in {repo}")
 
     plt.savefig(f"{path}/lines.png")
     plt.clf()
 
 
-def insertion_deletion_ratio(df_ai, df_no_ai, path):
+def insertion_deletion_ratio(df_ai, df_no_ai, path, repo):
     # Ratio of inserts and deletetions
 
     df_ai["ratio_insertion_deletion"] = df_ai["insertions"] / df_ai[
@@ -145,13 +145,13 @@ def insertion_deletion_ratio(df_ai, df_no_ai, path):
 
     plt.xticks([1, 2], ["AI", "Non-AI"])
     plt.ylabel("Insertion / Deletion Ratio")
-    plt.title("Distribution of Insertion/Deletion Ratio")
+    plt.title(f"Distribution of Insertion/Deletion Ratio in {repo}")
 
     plt.savefig(f"{path}/ratio.png")
     plt.clf()
 
 
-def insertions(df_ai, df_no_ai, path):
+def insertions(df_ai, df_no_ai, path, repo):
     # Insertions only
 
     df_AI = df_ai
@@ -175,13 +175,13 @@ def insertions(df_ai, df_no_ai, path):
 
     plt.xticks([1, 2], ["AI", "Non-AI"])
     plt.ylabel("Insertions")
-    plt.title("Distribution of insertions")
+    plt.title(f"Distribution of insertions in {repo}")
 
     plt.savefig(f"{path}/insertions.png")
     plt.clf()
 
 
-def deletions(df_ai, df_no_ai, path):
+def deletions(df_ai, df_no_ai, path, repo):
     # Deletions only
 
     df_AI = df_ai
@@ -204,13 +204,13 @@ def deletions(df_ai, df_no_ai, path):
 
     plt.xticks([1, 2], ["AI", "Non-AI"])
     plt.ylabel("Deletions")
-    plt.title("Distribution of deletions")
+    plt.title(f"Distribution of deletions in {repo}")
 
     plt.savefig(f"{path}/deletions.png")
     plt.clf()
 
 
-def reverted_commits(df_ai, df_no_ai, path):
+def reverted_commits(df_ai, df_no_ai, path, repo):
     count_ai = df_ai["is_reverted"].sum()
     count_no_ai = df_no_ai["is_reverted"].sum()
 
@@ -230,12 +230,12 @@ def reverted_commits(df_ai, df_no_ai, path):
         f.close()
 
     plt.ylabel("Number of Reverted Commits")
-    plt.title("Reverted Commits: AI vs Non-AI")
+    plt.title(f"Reverted Commits: AI vs Non-AI in {repo}")
     plt.savefig(f"{path}/reverted.png")
     plt.clf()
 
 
-def cc_plots(df_ai, df_no_ai, path):
+def cc_plots(df_ai, df_no_ai, path, repo):
     mean_value_AI = df_ai["delta_cc_norm"].mean()
     mean_value_NO_AI = df_no_ai["delta_cc_norm"].mean()
 
@@ -254,13 +254,13 @@ def cc_plots(df_ai, df_no_ai, path):
 
     plt.xticks([1, 2], ["AI", "Non-AI"])
     plt.ylabel("Cyclomatic Complexity")
-    plt.title("Distribution of Cyclomatic Complexity")
+    plt.title(f"Distribution of Cyclomatic Complexity in {repo}")
 
     plt.savefig(f"{path}/cc.png")
     plt.clf()
 
 
-def failed_pipelines(df_ai, df_no_ai, path):
+def failed_pipelines(df_ai, df_no_ai, path, repo):
     count_ai = df_ai["failed_pipeline"].sum()
     count_no_ai = df_no_ai["failed_pipeline"].sum()
 
@@ -280,7 +280,7 @@ def failed_pipelines(df_ai, df_no_ai, path):
         f.close()
 
     plt.ylabel("Number of Failed Pipelines")
-    plt.title("Failed Pipelines: AI vs Non-AI")
+    plt.title(f"Failed Pipelines: AI vs Non-AI in {repo}")
     plt.savefig(f"{path}/failed_pipelines.png")
     plt.clf()
 
@@ -304,22 +304,30 @@ def main():
             df_ai = pd.read_csv(path_ai)
             df_no_ai = pd.read_csv(path_no_ai)
 
-            mean_lines(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
-            insertion_deletion_ratio(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
-            insertions(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
-            deletions(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
-            reverted_commits(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
-            failed_pipelines(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
-            changes_in_30_days(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
-            duplication(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
-            files(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
+            mean_lines(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos)
+            insertion_deletion_ratio(
+                df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos
+            )
+            insertions(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos)
+            deletions(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos)
+            reverted_commits(
+                df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos
+            )
+            failed_pipelines(
+                df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos
+            )
+            changes_in_30_days(
+                df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos
+            )
+            duplication(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos)
+            files(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos)
 
             path_ai = f"csv/{language}/{repos}/cc_results_AI.csv"
             path_no_ai = f"csv/{language}/{repos}/cc_results_nonAI.csv"
             df_ai = pd.read_csv(path_ai)
             df_no_ai = pd.read_csv(path_no_ai)
 
-            cc_plots(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path)
+            cc_plots(df_ai=df_ai, df_no_ai=df_no_ai, path=output_path, repo=repos)
 
 
 if __name__ == "__main__":
